3cf3b4694a757a27bca4550189c9ae0b1641fd42
Yonggan <yonggan@oblivioncoding.pro>
2021-10-08 16:30:12 +0200

    Add Authorization

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 
-var app = express();
-var db = require('./src/db/db')
+const app = express();
+const db = require('./src/db/db')
 const dotenv = require('dotenv');
 
 // get config vars
 dotenv.config();
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const https = require('https');
 
 router.post('/login', (req, res) => {
-    const {api_secret} = req.body;
-    if (api_secret === process.env.API_SECRET) {
-        const token = generateAccessToken({clientId: api_secret});
-        res.status(201).json({token: token});
-    } else {
-        res.status(403).send("Wrong Secrets");
-    }
+    const {auth_token} = req.body;
+    https.get({
+        host: "www.googleapis.com",
+        path: "/oauth2/v1/tokeninfo",
+        headers: {access_token: auth_token}
+    }, function (result) {
+        if(result.statusCode === 200){
+            const token = generateAccessToken({apiSecret: auth_token});
+            res.status(200).json({token: token});
+        }else{
+            res.status(401).send("Invalid access Token");
+        }
+    });
 });
 
 function generateAccessToken(username) {
