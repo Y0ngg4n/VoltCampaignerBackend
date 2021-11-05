5a4a38c4341f498448f1d9f4ef013b250940902c
Yonggan <yonggan@oblivioncoding.pro>
2021-11-05 12:04:56 +0100

    Add import

--- routes/poster.js
+++ routes/poster.js
@@ -25,8 +25,30 @@ router.post('/create', auth, async (req, res) => {
 
 router.post('/update', auth, async (req, res) => {
     try {
-        const {id, hanging, latitude, longitude, campaign, poster_type, motive, target_groups, environment, other} = req.body;
-        const poster = {id, hanging, latitude, longitude, campaign, poster_type, motive, target_groups, environment, other};
+        const {
+            id,
+            hanging,
+            latitude,
+            longitude,
+            campaign,
+            poster_type,
+            motive,
+            target_groups,
+            environment,
+            other
+        } = req.body;
+        const poster = {
+            id,
+            hanging,
+            latitude,
+            longitude,
+            campaign,
+            poster_type,
+            motive,
+            target_groups,
+            environment,
+            other
+        };
         const client = await db.getConnection();
         await poster_db.updatePoster(client, poster, (err, result) => {
             db.disconnect(client);
@@ -70,6 +92,37 @@ router.get('/all', auth, async (req, res) => {
                 return res.status(401).send({error: err.message});
             } else {
                 return res.status(200).send(result.rows);
+            }
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send(error);
+    }
+});
+
+router.post('/import', auth, async (req, res) => {
+    try {
+        const client = await db.getConnection();
+        await db.retryTxn(0, 15, client, async (client, callback) => {
+            for (let element in req.body) {
+                const poster = {
+                    latitude: element.latitude,
+                    longitude: element.longitude,
+                    campaign: element.campaign,
+                    poster_type: element.poster_type,
+                    motive: element.motive,
+                    target_groups: element.target_groups,
+                    environment: element.environment,
+                    other: element.other
+                };
+                await poster_db.createPoster(client, poster, callback)
+            }
+        }, (err, result) => {
+            db.disconnect(client);
+            if (err) {
+                return res.status(401).send({error: err.message});
+            } else {
+                return res.status(201).send();
             }
         })
     } catch (error) {
