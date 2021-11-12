363cce7c18c51d04a735a77e2f909254e7694e26
Yonggan <yonggan@oblivioncoding.pro>
2021-11-12 11:14:29 +0100

    Add Debug for poster

--- routes/poster.js
+++ routes/poster.js
@@ -67,9 +67,16 @@ router.post('/update', auth, async (req, res) => {
 router.get('/distance', auth, async (req, res) => {
     try {
         const {latitude, longitude, distance, hanging, last_update} = req.headers;
+        console.log("Get Poster for:")
+        console.log("Latitude: "+ latitude)
+        console.log("Longitude: "+ longitude)
+        console.log("distance: "+ distance)
+        console.log("hanging: "+ hanging)
+        console.log("last_update: "+ last_update)
         const client = await db.getConnection();
         await poster_db.getPosterInMeterRange(client, latitude, longitude, distance, hanging, last_update, (err, result) => {
             db.disconnect(client);
+            console.log(result);
             if (err) {
                 return res.status(401).send({error: err.message});
             } else {
